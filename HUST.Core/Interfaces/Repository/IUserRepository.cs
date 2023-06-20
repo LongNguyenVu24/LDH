e7146ce1a634294e66912c6e0d284d39adf38999
LongNguyenVu24 <86005133+LongNguyenVu24@users.noreply.github.com>
2023-06-20 22:31:13 +0700

    hung day duy con

---
+++ HUST.Core/Interfaces/Repository/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using HUST.Core.Models.DTO;
+using HUST.Core.Models.Entity;
+using System;
+using System.Threading.Tasks;
+
+namespace HUST.Core.Interfaces.Repository
+{
+
+    public interface IUserRepository: IBaseRepository<user>
+    {
+        /// <summary>
+        /// Khởi tạo dữ liệu khi tài khoản được kích hoạt
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        Task<bool> CreateActivatedAccountData(string userId);
+    }
+}
